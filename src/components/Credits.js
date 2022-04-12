882327fa379ba967181ca8b49981e8abd4fde271
Kevin Hinh <kevinhinh9@gmail.com>
2022-04-12 19:04:21 -0400

    addCredits Implemented

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,11 +1,12 @@
 // src/components/Credits.js
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Component } from 'react';
 
 const Credits = (props) => {
     function FetchCredits() {
         const [creditData, setCreditData] = useState([{
             description: '',
             amount: '',
+            date: '',
         }])
 
         useEffect(() => {fetch('https://moj-api.herokuapp.com/credits')
@@ -16,16 +17,18 @@ const Credits = (props) => {
             }).then(jsonRes => setCreditData(jsonRes));
         }, [])
 
+
         return(
             <div className='creditsContainer'>
                 {creditData.map((info) => {
                     return (
                         <>
                             <div className='creditsCard'>
-                                <div className='infoDiv'>{info.id}</div>
                                 <div className='infoDiv'>{info.description}</div>
                                 <div className='infoDiv'>{info.amount}</div>
+                                <div className='infoDiv'>{info.date.slice(0,10)}</div>
                             </div>
+                            <br></br>
                         </>
                     )
                 })}
@@ -33,15 +36,89 @@ const Credits = (props) => {
         )
     }
 
+    function AddCredit(newEntry) {
+        console.log(newEntry);
+        return(
+            <div>
+                <div className='infoDiv'>{newEntry.description}</div>
+                <div className='infoDiv'>{newEntry.amount}</div>
+                <div className='infoDiv'>{newEntry.date}</div>
+            </div>
+        )
+    }
+
+    function getDate() {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        
+        today = yyyy + '-' + mm + '-' + dd;
+        return today;
+    }
+
+    class AddCredits extends Component {
+        constructor() {
+            super();
+            this.state = {
+                description: '',
+                amount: '',
+                date: '',
+            }
+
+            this.changeDescription = this.changeDescription.bind(this);
+            this.changeAmount = this.changeAmount.bind(this);
+            this.onSubmit = this.onSubmit.bind(this);
+        }
+
+        changeDescription(event) {
+            this.setState({
+                description:event.target.value
+            })
+        }
+        
+        changeAmount(event) {
+            this.setState({
+                amount:event.target.value
+            })
+        }
+
+        onSubmit(event) {
+            event.preventDefault();
+
+            const creditsInfo = {
+                description: this.state.description,
+                amount: this.state.amount,
+                date: getDate(),
+            }
+
+            AddCredit(creditsInfo);
+
+            this.setState({
+                description:'',
+                amount:'',
+            })
+        }
+
+        render() {
+            return (
+                <div>
+                    <form onSubmit={this.onSubmit}>
+                        <input type="text" placeholder="Description" name="description" onChange={this.changeDescription} value={this.state.description}/>
+                        <input type="number" placeholder="Amount" name="amount" onChange={this.changeAmount} value={this.state.amount}/>
+                        <button type="submit">Add Credit</button>
+                    </form>
+                </div>
+            )
+        }
+    }
+
   return (
     <div>
       <h1>Credits</h1>
-      <form onSubmit={props.addCredit}>
-        <input type="text" placeholder="Description" name="description" />
-        <input type="number" placeholder="Amount" name="amount" />
-        <button type="submit">Add Credit</button>
-      </form>
+      <AddCredits/>
       <FetchCredits/>
+      <AddCredit/>
     </div>
   )
 }
